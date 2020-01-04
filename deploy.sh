586b96298ea3aa60bbd28a44036509770e693d90
Michael Aranovich <mikea@itsiren.com>
2020-01-04 14:20:15 -0600

    final update

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t mike2help/multi-client:latest -t mike2help/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mike2help/multi-server:latest -t mike2help/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mike2help/multi-worker:latest -t mike2help/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push mike2help/multi-client:latest
+docker push mike2help/multi-server:latest
+docker push mike2help/multi-worker:latest
+
+docker push mike2help/multi-client:$SHA
+docker push mike2help/multi-server:$SHA
+docker push mike2help/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=mike2help/multi-server:$SHA
+kubectl set image deployments/client-deployments client=mike2help/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=mike2help/multi-worker:$SHA
+
