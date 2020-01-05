777eecaabeaa7f895ef6421b4d00cc9bb8010be6
Michael Aranovich <mikea@itsiren.com>
2020-01-05 11:00:01 -0600

    typo fix "deploymets - to - deployment"

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t mike2help/multi-client:latest -t mike2help/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t mike2help/multi-server:latest -t mike2help/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t mike2help/multi-worker:latest -t mike2help/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push mike2help/multi-client:latest
 docker push mike2help/multi-server:latest
 docker push mike2help/multi-worker:latest
@@ -8,8 +9,9 @@ docker push mike2help/multi-worker:latest
 docker push mike2help/multi-client:$SHA
 docker push mike2help/multi-server:$SHA
 docker push mike2help/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=mike2help/multi-server:$SHA
-kubectl set image deployments/client-deployments client=mike2help/multi-client:$SHA
-kubectl set image deployments/worker-deployments worker=mike2help/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=mike2help/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mike2help/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mike2help/multi-worker:$SHA
 
